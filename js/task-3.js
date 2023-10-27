dc47591dd5fc788d30685474592169470b9b3db0
ValeriiaProkhina <140752336+ValeriiaProkhina@users.noreply.github.com>
2023-10-27 21:32:17 +0300

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,16 @@
+'use strict';
+function checkForSpam(message) {
+  const substringSpam = 'spam';
+  const substringSale = 'sale';
+  const result =
+    message.toLowerCase().includes(substringSpam) ||
+    message.toLowerCase().includes(substringSale);
+  return result;
+}
+console.log(checkForSpam('Latest technology news'));
+console.log(checkForSpam('JavaScript weekly newsletter'));
+console.log(checkForSpam('Get best sale offers now!'));
+console.log(checkForSpam('Amazing SalE, only tonight!'));
+console.log(checkForSpam('Trust me, this is not a spam message'));
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!'));
+console.log(checkForSpam('[SPAM] How to earn fast money?'));
